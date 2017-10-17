9a0adf9776816c0c3bb42be997fb1b9f827ae44e
Adam Yi <xuan@yiad.am>
2017-10-17 22:44:52 +0800

    Separate REVISION file
    This commit fixes issue #5

--- src/generate_versions.py
+++ src/generate_versions.py
@@ -2,10 +2,11 @@
 # -*- coding: UTF-8 -*- 
 
 fo_src = open("src/cv.tex", "r")
+fo_rev = open("REVISION", "r")
 fo_print = open("build/cv_print.tex", "w")
 fo_screen = open("build/cv_screen.tex", "w")
 try:
-    content = fo_src.read()
+    content = fo_src.read().replace('__REVISION__', fo_rev.read().strip())
     fo_print.write(content.replace('__VERSION__', ',print'))
     fo_screen.write(content.replace('__VERSION__', ''))
 finally:
