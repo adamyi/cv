2d8eed782410eca3db9b1069d92ee17808770161
Adam Yi <xuan@yiad.am>
2017-11-15 12:10:34 +0800

    jenkins fix

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,15 @@
 CV_REV=$((`cat REVISION`+1))
 echo $CV_REV > REVISION
 make both
-git clone https://github.com/adamyi/yiad.am
+git clone git@github.com:adamyi/yiad.am
 cp cv_screen.pdf yiad.am/src/cv.pdf
 cd yiad.am
 ./update_cv.sh $CV_REV
 git commit -am "Auto update CV to rev $CV_REV"
-git push
+git push origin master
 cd ..
-\rm -r yiad.am
+\rm -rf yiad.am
 git add *.pdf
 git add REVISION
 git commit -m "Auto bump to rev $CV_REV"
-git push
+git push origin master
