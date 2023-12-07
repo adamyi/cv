7359553cb29574ad0b979f98bea6e48bdc916ec6
Adam Yi <i@adamyi.com>
2023-12-08 01:28:58 +0800

    fix python bytes conversion

--- src/generate_versions.py
+++ src/generate_versions.py
@@ -6,7 +6,7 @@ fo_src = open("src/cv.tex", "r")
 # fo_rev = open("REVISION", "r")
 fo_print = open("build/cv_print.tex", "w")
 fo_screen = open("build/cv_screen.tex", "w")
-revision = str(subprocess.check_output(["git", "describe", "--dirty"]).strip()) + "-n"
+revision = subprocess.check_output(["git", "describe", "--dirty"]).strip().decode("utf-8") + "-n"
 try:
     content = fo_src.read().replace('__REVISION__', revision)
     fo_print.write(content.replace('__VERSION__', ',print'))
